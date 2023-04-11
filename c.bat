c783e5b246faafb683cd770a0c0cb3b392b8c938
DevIssaAb <issaab.dev@gmail.com>
2023-04-11 15:49:52 +0300

    AA

--- c.bat
+++ c.bat
@@ -1,6 +1,13 @@
 setlocal EnableDelayedExpansion
 
+echo hi >> "C:\Users\modar\Desktop\Test101\test\sql\sql1.txt"
+echo hi >> "C:\Users\modar\Desktop\Test101\test\sql\sql2.txt"
+echo hi >> "C:\Users\modar\Desktop\Test101\test\sql\sql3.txt"
 
+git add test\sql\sql1.txt
+git add test\sql\sql2.txt
+git add test\sql\sql3.txt
 
-git add .
+git commit -am "AA"
+git push
 
