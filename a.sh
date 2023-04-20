4d4883cbd03b34b829167874da0e750c408ba1cd
DevIssaAb <issaab.dev@gmail.com>
2023-04-20 15:25:35 +0300

    AA

--- a.sh
+++
@@ -1,8 +0,0 @@
-# git diff 3ea6437..8395a7b -U0 | grep '^[+-]' | grep -Ev '^(--- a/|\+\+\+ b/)'
-
-git log -n 1  --pretty=format: -p | grep '^[+-]' | grep -Ev '^(--- a/|\+\+\+ b/)'
-
-author=$(git log -n 1  --pretty=format: -p | grep '^[+-]' | grep -Ev '^(\+\+\+ b/)')
-echo $author
-
-sleep 100
