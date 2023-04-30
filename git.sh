0607e316fefd7149d88abf298aad7c782cd8196b
DevIssaAb <issaab.dev@gmail.com>
2023-04-30 13:42:57 +0300

    Test Change Version From Issa

--- git.sh
+++ git.sh
@@ -1,9 +1,7 @@
 git add test.txt
 git add sub.txt
-git add "a.txt"
-git add "b.txt"
 git add "test\a_b\create_strategy.cpp"
 git add CheckCahnge.py
-#git add git.sh
-git commit -m "Test Change Version"
-git push
+git add git.sh
+git commit -m "Test Change Version From Issa"
+git push origin issa
