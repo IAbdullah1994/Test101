180b628521a314d72bfa3b72ed42ed603823bf1d
DevIssaAb <issaab.dev@gmail.com>
2023-05-01 15:47:05 +0300

    DA

--- a.sh
+++ a.sh
@@ -1,6 +1,10 @@
-ChangeLog=$(git log -n 1  --pretty=format: -p | grep  '^[diff+-]' | grep -Ev '/dev/null|^(--- a/|\+\+\+ b/)')
-NameFiles=$(git log -n 1 --pretty="format:" --name-only)
-LastID=$(git log -n 1 --pretty=format:%h -- )
+LastID=$(git for-each-ref refs/remotes/origin --sort="-committerdate" --format="%(objectname)" | head -1)
+
+
+
+ChangeLog=$(git log -n 1  --pretty=format: -p $LastID | grep  '^[diff+-]' | grep -Ev '/dev/null|^(--- a/|\+\+\+ b/)')
+NameFiles=$(git log -n 1 --pretty="format:" --name-only $LastID)
+#LastID=$(git log -n 1 --pretty=format:%h -- )
 echo "$ChangeLog" > ChangeLog.txt
 
 
@@ -11,11 +15,11 @@ python CheckCahnge.py "$NameFiles" $ResultLog "ChangeLog.txt" "$LastID"
 
 if test -f "$ResultLog"; then  
 value="$(<$ResultLog)"
-author=$(git log -n 1 --pretty=format:%an -- )
+author=$(git log -n 1 --pretty=format:%an  $LastID)
 # echo $author
 gh issue create --title "Consider incrementing minor version" --body "$value" -a "$author"
 rm $ResultLog
 fi 
 rm ChangeLog.txt
 
-#sleep 10
+sleep 10
