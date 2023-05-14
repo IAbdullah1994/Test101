ea134e39c2a03db8d146f93986d48b9f7557e443
DevIssaAb <issaab.dev@gmail.com>
2023-05-14 13:47:50 +0300

    Test Change Version From Master

---
+++ CheckChange.py
@@ -0,0 +1,62 @@
+import argparse
+
+NameFiles = []
+diff="diff --git "
+version="version"
+
+# Instantiate the parser
+# Get the changed files
+parser = argparse.ArgumentParser(description='Optional app description')
+parser.add_argument('NameFiles', help='Change log')
+parser.add_argument('ResultLog')
+parser.add_argument('ChangeLog')
+args = parser.parse_args()
+
+NameFiles = str(args.NameFiles).split('\n')
+NameFiles = set([x for x in NameFiles if x])
+
+ResultLog = args.ResultLog
+ChangeLog = args.ChangeLog
+
+    
+# Read log Change for Last commit
+f = open(f"{ChangeLog}", "r")
+ChangeLog =f.readlines()
+file=""
+is_version=False
+resultdic={}
+id=""
+for log in ChangeLog:
+    if log.startswith("diff --gitid:"):
+       id=log.split(':')[1].replace('\n','')
+    if log.startswith(diff) : 
+        for NameFile in NameFiles:
+            if NameFile in log :
+                  file=NameFile
+                  if not file in resultdic.keys():
+                      resultdic[file]=""
+                  break
+        is_version=False       
+        continue
+    if is_version: continue
+    if version in log.lower():
+        is_version=True
+        resultdic[file]+=f"{id} "
+        continue
+f.close()
+
+resultdic={k: v for k, v in resultdic.items() if v}
+print(resultdic)
+if len(resultdic) != 0:
+    f = open(f"{ResultLog}", "w")
+    f.write("These files were changed:  \n")
+    [f.write(f'{k} {v} \n') for k, v in resultdic.items() ]
+    f.close()
+    
+     
+
+
+
+# For delete Emtpy item/s
+#result=[x for x in result if x]
+#result=[x.replace('/dev/null','') for x in result if x]
--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ git add a.sh
 git add b.sh
 git add "test\a_b\create_strategy.cpp"
 git add "test\a_b\b.txt"
-git add CheckCahnge.py
+git add CheckChange.py
 git add git.sh
 git commit -m "Test Change Version From Master"
 git push origin master
