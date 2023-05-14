77b84222789d77a8913f1f0e17751eb83f31215f
DevIssaAb <issaab.dev@gmail.com>
2023-05-14 14:11:16 +0300

    Test Change Version From Master

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@ git add test.txt
 git add sub.txt
 git add a.sh
 git add b.sh
-git add "test\sql\*.txt"
+git add "test\sql\*.sql"
 git add "test\a_b\create_strategy.cpp"
 git add "test\a_b\b.txt"
 git add CheckChange.py
