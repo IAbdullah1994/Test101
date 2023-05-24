f40b0bf97ebbe07e315854cd630115b42361d5c6
DevIssaAb <issaab.dev@gmail.com>
2023-05-24 11:41:08 +0300

    Test Change Version From Master

--- jenkins/CheckChange.py
+++ jenkins/CheckChange.py
@@ -40,10 +40,13 @@ parser.add_argument('ChangeLog')
 args = parser.parse_args()
 
 # NameFiles = str(args.NameFiles).split('\n')
+# NameFiles = set([x for x in NameFiles if x])
+
+
 NameFiles = args.NameFiles
 f = open(f"{NameFiles}", "r", errors="ignore")
 NameFiles = f.readlines()
-NameFiles = set([x.replace("\n","") for x in NameFiles if x])
+NameFiles = set([x for x in NameFiles if x])
 f.close()
 
 ResultLog = args.ResultLog
