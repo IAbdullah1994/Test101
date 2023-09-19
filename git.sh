1aa2ffe198aca01db715b8591b9dafebb3d540bd
IAbdullah1994 <i.abdullah@masah.tech>
2023-09-19 11:19:48 +0300

    Test Change Version From Master

--- git.sh
+++ git.sh
@@ -2,6 +2,7 @@ git add test.txt
 git add sub.txt
 git add UpdateBranchDicScript.sh
 git add VersionChangesScript.sh
+git add "data\system\reports\*.*"
 git add "test\sql\*.sql"
 git add "test\a_b\create_strategy.cpp"
 git add "test\a_b\b.txt"
